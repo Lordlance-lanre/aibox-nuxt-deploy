edb88976fbb2a9da42d1597010bdf5e57fb8451d
Lordlance-lanre <omosehinolanrewaju@gmail.com>
2023-03-07 19:03:21 +0100

    first commit

---
+++ components/LoginInfo.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="mt-14">
+	<div class="block mx-24 md:mx-96 justify-center">
+		<label class="block mb-2 text-sm font-medium text-white">Username</label>
+		<input type="text" class="w-72 md:w-11/12 py-4 rounded-lg border-sky-500 px-3 outline-sky-500" placeholder="Enter Username">
+	</div>
+
+	<div class="mt-7 block mx-24 md:mx-96 justify-center">
+		<label class="block mb-2 text-sm font-medium text-white">Password</label>
+		<input type="text" class="w-72 md:w-11/12 py-4 rounded-lg border-black px-3" placeholder="Enter Password">
+		<div class="absolute -mt-10 mx-64 md:mx-96">
+			 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  			<path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+			</svg>
+		</div>
+	</div>
+
+	<div class="flex justify-end mx-16 mt-5 md:hidden">
+		<h4 class="text-white">Forgot Password ?</h4>
+	</div>
+
+	<div class="mt-7 md:mt-16">
+		<button class="flex mx-auto bg-white px-12 py-3 rounded-full text-indigo-600 hover:bg-indigo-900 font-medium hover:text-white">Login</button>
+		<div class="absolute -mt-9 md:text-white text-indigo-600 mx-64 md:hidden hover:text-white">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:text-white">
+  			<path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+			</svg>
+
+		</div>
+	</div>
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="bg-indigo-600 pt-20 md:w-screen  h-screen">
+  <div id="top" class="text-center text-white text-6xl font-medium tracking-wide">
+    tutu
+  </div>
+  <div class="text-center text-white mt-3 font-light tracking-wide">
+    <p>Welcome to tutu-wallets for all</p>
+  </div>
+
+  <div>
+    <LoginInfo/>
+  </div>
+ </div>
+</template>
+
+<script setup>
+import { ref } from '@nuxtjs/composition-api';
+import LoginInfo from '../components/LoginInfo.vue'
+
+const newPage = ref('Yes');
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap');
+#top{
+  font-family: Fredoka One;
+}
+</style>
